625ed2d66407e1c12fbfbff08524070a67fd14fc
Patryk-Zygmunt <patrykzygmuntzx@gmail.com>
2019-10-28 17:12:07 +0100

    vgvg vg

--- src/main/scala/FordFulkerson.scala
+++ src/main/scala/FordFulkerson.scala
@@ -4,12 +4,12 @@ object FordFulkerson {
 
 
   def main(args: Array[String]): Unit = {
-
+    println(findPathRec(Array(0,0,1,0,2),0,2).deep.mkString(", "))
   }
 
 
 
-  def bfs(graph:Array[Node], s:Int , e:Int): Array[Node] = {
+  def bfs(graph:Array[Node], s:Int , e:Int): Array[Int] = {
     var visited = Array[Boolean]()
     var queue = mutable.Queue[Node]()
     var parent = Array[Int]()
@@ -17,6 +17,7 @@ object FordFulkerson {
     visited(0) = true
     while(queue.nonEmpty) {
       val node = queue.dequeue()
+      if(node.i==e) return parent;
       if (!visited(node.i)) {
         node.nodes.foreach {
           case (id: Int, distance: Int) => {
@@ -27,8 +28,16 @@ object FordFulkerson {
         visited(node.i)
       }
     }
+    return null
   }
 
 
+
+
+
+  def findPathRec(parent:Array[Int], s:Int,nodeId:Int):Array[Int]={
+    if(nodeId == s) return Array[Int](nodeId)
+    else findPathRec(parent,s,parent(nodeId)) :+ nodeId
+  }
 
 }
